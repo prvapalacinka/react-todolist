d83f0270ed299fed93a2872d698f47205e0cfb2a
Broly <vidovicp20@gmail.com>
2023-01-10 18:02:31 -0500

    installed react, added TodoList JS and todo JS files and applied useState

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import React, {useState} from 'react';
+import './App.css';
+import TodoList from './TodoList';
+function App() {
+  const [todos, setTodos] = useState(['Todo1', 'Todo2'])
+  return (
+    <>
+    <TodoList todos={todos}/>
+    <input type="text"/>
+    <button>Add Todo</button>
+    <button>Clear Completed Todos</button>
+    <div>0 left to do</div>
+    </>
+  );
+}
+
+export default App;
