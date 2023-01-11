d08fc988367a351bf7401ddfea9be42f2cd1ffc7
Broly <vidovicp20@gmail.com>
2023-01-11 18:51:12 -0500

    fixed todos list

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 
-export default function TodoList() {
-    const [todos, setTodos] =useState ([])
+export default function TodoList({todos}) {
   return (
-  <></>
+  <>
+  <div>{todos.length}</div>
+  </>
   )
 };
